e59f5ac7ff0bd81b7805d686daf42f7549b2b426
Timo Tschupp <timo.tschupp@ukg.com>
2022-09-12 11:04:01 -0400

    Fixed Bug "Exception thrown calling BulkInsert if AutoMapOutputIdentity is true and the class has more than one key property" Added test case With_Complex_Key to BulkInsert.cs Added test case With_Complex_Key to BulkInsertAsync.cs Added FilterColumns to CommonUtil.cs to filter columns for BulkInsert operations Added GetDBValue to SqlUtil to handle values pulled from the database (DBNull will be converted to null)

--- N.EntityFrameworkCore.Extensions.Test/Data/TestDbContext.cs
+++ N.EntityFrameworkCore.Extensions.Test/Data/TestDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using N.EntityFrameworkCore.Extensions.Test.Common;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.ConstrainedExecution;
 
 namespace N.EntityFrameworkCore.Extensions.Test.Data
 {
     public class TestDbContext : DbContext
     {
         public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductWithComplexKey> ProductsWithComplexKey { get; set; }
         public virtual DbSet<Order> Orders { get; set;  }
         public virtual DbSet<TphPerson> TphPeople { get; set; }
         public virtual DbSet<TphCustomer> TphCustomers { get; set; }
@@ -20,6 +23,10 @@ namespace N.EntityFrameworkCore.Extensions.Test.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TphPerson>().Property<DateTime>("CreatedDate");
+            modelBuilder.Entity<ProductWithComplexKey>().HasKey(c => new { c.Key1 });
+            modelBuilder.Entity<ProductWithComplexKey>().Property<Guid>("Key1").HasDefaultValueSql("newsequentialid()");
+            modelBuilder.Entity<ProductWithComplexKey>().Property<Guid>("Key2").HasDefaultValueSql("newsequentialid()");
+            modelBuilder.Entity<ProductWithComplexKey>().Property<Guid>("Key3").HasDefaultValueSql("newsequentialid()");
         }
     }
 }
